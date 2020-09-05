f7db23c412d87c4bd422078637af1021d803c59b
sudip subedi <sudipsubedi.np@gmail.com>
2020-09-05 09:34:56 +0545

    Update macsetup.sh

--- macsetup.sh
+++ macsetup.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
 
+#install mac software from homebrew
+brew cask install google-chrome
+brew cask install alfred
+brew cask install firefox
+brew cask install skype
+brew cask install slack
+brew cask install kap
