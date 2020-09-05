31fadeea0606a040eec12232cbe8a6d30b53e470
sudip subedi <sudipsubedi.np@gmail.com>
2020-09-05 09:46:46 +0545

    Update macsetup.sh

--- macsetup.sh
+++ macsetup.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Install Brew Packages
+brew install git tree python yarn
+
+#install node
+echo "installing node (via n-install)"
+curl -L https://git.io/n-install | bash
+
+echo "installing apps with brew cask"
+
 #install mac software from homebrew
 brew cask install google-chrome
 brew cask install alfred
@@ -7,3 +16,5 @@ brew cask install firefox
 brew cask install skype
 brew cask install slack
 brew cask install kap
+brew cask install visual-studio-code
+
